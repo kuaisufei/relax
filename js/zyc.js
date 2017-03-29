6297fb747cd46d4f616869909d834ec0c74be696
e <2859941342@qq.com>
2017-03-29 14:28:36 +0800

    Signed-off-by: zhangyuchao <2859941342@qq.com>

--- js/zyc.js
+++ js/zyc.js
@@ -17,91 +17,130 @@ var handlerfunc = {
 //数字处理
 var numberhandle = {
 	//数字转换成人民币汉字
-	numbertormb: function(money) {    
-	  //汉字的数字
-	  var cnNums = new Array('零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖');
-	  //基本单位
-	  var cnIntRadice = new Array('', '拾', '佰', '仟');
-	  //对应整数部分扩展单位
-	  var cnIntUnits = new Array('', '万', '亿', '兆');
-	  //对应小数部分单位
-	  var cnDecUnits = new Array('角', '分', '毫', '厘');
-	  //整数金额时后面跟的字符
-	  var cnInteger = '整';
-	  //整型完以后的单位
-	  var cnIntLast = '元';
-	  //最大处理的数字
-	  var maxNum = 999999999999999.9999;
-	  //金额整数部分
-	  var integerNum;
-	  //金额小数部分
-	  var decimalNum;
-	  //输出的中文金额字符串
-	  var chineseStr = '';
-	  //分离金额后用的数组，预定义
-	  var parts;
-	  if (money == '') { return ''; }
-	  money = parseFloat(money);
-	  if (money >= maxNum) {
-	    //超出最大处理数字
-	    return '';
-	  }
-	  if (money == 0) {
-	    chineseStr = cnNums[0] + cnIntLast + cnInteger;
-	    return chineseStr;
-	  }
-	  //转换为字符串
-	  money = money.toString();
-	  if (money.indexOf('.') == -1) {
-	    integerNum = money;
-	    decimalNum = '';
-	  } else {
-	    parts = money.split('.');
-	    integerNum = parts[0];
-	    decimalNum = parts[1].substr(0, 4);
-	  }
-	  //获取整型部分转换
-	  if (parseInt(integerNum, 10) > 0) {
-	    var zeroCount = 0;
-	    var IntLen = integerNum.length;
-	    for (var i = 0; i < IntLen; i++) {
-	      var n = integerNum.substr(i, 1);
-	      var p = IntLen - i - 1;
-	      var q = p / 4;
-	      var m = p % 4;
-	      if (n == '0') {
-	        zeroCount++;
-	      } else {
-	        if (zeroCount > 0) {
-	          chineseStr += cnNums[0];
-	        }
-	        //归零
-	        zeroCount = 0;
-	        chineseStr += cnNums[parseInt(n)] + cnIntRadice[m];
-	      }
-	      if (m == 0 && zeroCount < 4) {
-	        chineseStr += cnIntUnits[q];
-	      }
-	    }
-	    chineseStr += cnIntLast;
-	  }
-	  //小数部分
-	  if (decimalNum != '') {
-	    var decLen = decimalNum.length;
-	    for (var i = 0; i < decLen; i++) {
-	      var n = decimalNum.substr(i, 1);
-	      if (n != '0') {
-	        chineseStr += cnNums[Number(n)] + cnDecUnits[i];
-	      }
-	    }
-	  }
-	  if (chineseStr == '') {
-	    chineseStr += cnNums[0] + cnIntLast + cnInteger;
-	  } else if (decimalNum == '') {
-	    chineseStr += cnInteger;
-	  }
-	  return chineseStr;
-	}
+	numbertormb: function(currencyDigits) { 
+	// Constants:
+	    var MAXIMUM_NUMBER = 99999999999.99; 
+	    // Predefine the radix characters and currency symbols for output:
+	    var CN_ZERO = "零"; 
+	    var CN_ONE = "壹"; 
+	    var CN_TWO = "贰"; 
+	    var CN_THREE = "叁"; 
+	    var CN_FOUR = "肆"; 
+	    var CN_FIVE = "伍"; 
+	    var CN_SIX = "陆"; 
+	    var CN_SEVEN = "柒"; 
+	    var CN_EIGHT = "捌"; 
+	    var CN_NINE = "玖"; 
+	    var CN_TEN = "拾"; 
+	    var CN_HUNDRED = "佰"; 
+	    var CN_THOUSAND = "仟"; 
+	    var CN_TEN_THOUSAND = "万"; 
+	    var CN_HUNDRED_MILLION = "亿"; 
+	    var CN_SYMBOL = "人民币"; 
+	    var CN_DOLLAR = "元"; 
+	    var CN_TEN_CENT = "角"; 
+	    var CN_CENT = "分"; 
+	    var CN_INTEGER = "整"; 
+	     
+	// Variables:
+	    var integral;    // Represent integral part of digit number.
+	    var decimal;    // Represent decimal part of digit number.
+	    var outputCharacters;    // The output result.
+	    var parts; 
+	    var digits, radices, bigRadices, decimals; 
+	    var zeroCount; 
+	    var i, p, d; 
+	    var quotient, modulus; 
+	     
+	// Validate input string:
+	    currencyDigits = currencyDigits.toString(); 
+	    if (currencyDigits == "") { 
+	        alert("请输入小写金额！"); 
+	        return ""; 
+	    } 
+	    if (currencyDigits.match(/[^,.\d]/) != null) { 
+	        alert("小写金额含有无效字符！"); 
+	        return ""; 
+	    } 
+	    if ((currencyDigits).match(/^((\d{1,3}(,\d{3})*(.((\d{3},)*\d{1,3}))?)|(\d+(.\d+)?))$/) == null) { 
+	        alert("小写金额的格式不正确！"); 
+	        return ""; 
+	    } 
+	     
+	// Normalize the format of input digits:
+	    currencyDigits = currencyDigits.replace(/,/g, "");    // Remove comma delimiters.
+	    currencyDigits = currencyDigits.replace(/^0+/, "");    // Trim zeros at the beginning.
+	    // Assert the number is not greater than the maximum number.
+	    if (Number(currencyDigits) > MAXIMUM_NUMBER) { 
+	        alert("金额过大，应小于1000亿元！"); 
+	        return ""; 
+	    } 
+	     
+	// Process the coversion from currency digits to characters:
+	    // Separate integral and decimal parts before processing coversion:
+	    parts = currencyDigits.split("."); 
+	    if (parts.length > 1) { 
+	        integral = parts[0]; 
+	        decimal = parts[1]; 
+	        // Cut down redundant decimal digits that are after the second.
+	        decimal = decimal.substr(0, 2); 
+	    } 
+	    else { 
+	        integral = parts[0]; 
+	        decimal = ""; 
+	    } 
+	    // Prepare the characters corresponding to the digits:
+	    digits = new Array(CN_ZERO, CN_ONE, CN_TWO, CN_THREE, CN_FOUR, CN_FIVE, CN_SIX, CN_SEVEN, CN_EIGHT, CN_NINE); 
+	    radices = new Array("", CN_TEN, CN_HUNDRED, CN_THOUSAND); 
+	    bigRadices = new Array("", CN_TEN_THOUSAND, CN_HUNDRED_MILLION); 
+	    decimals = new Array(CN_TEN_CENT, CN_CENT); 
+	    // Start processing:
+	    outputCharacters = ""; 
+	    // Process integral part if it is larger than 0:
+	    if (Number(integral) > 0) { 
+	        zeroCount = 0; 
+	        for (i = 0; i < integral.length; i++) { 
+	            p = integral.length - i - 1; 
+	            d = integral.substr(i, 1); 
+	            quotient = p / 4; 
+	            modulus = p % 4; 
+	            if (d == "0") { 
+	                zeroCount++; 
+	            } 
+	            else { 
+	                if (zeroCount > 0) 
+	                { 
+	                    outputCharacters += digits[0]; 
+	                } 
+	                zeroCount = 0; 
+	                outputCharacters += digits[Number(d)] + radices[modulus]; 
+	            } 
+	            if (modulus == 0 && zeroCount < 4) { 
+	                outputCharacters += bigRadices[quotient]; 
+	                zeroCount = 0; 
+	            } 
+	        } 
+	        outputCharacters += CN_DOLLAR; 
+	    } 
+	    // Process decimal part if there is:
+	    if (decimal != "") { 
+	        for (i = 0; i < decimal.length; i++) { 
+	            d = decimal.substr(i, 1); 
+	            if (d != "0") { 
+	                outputCharacters += digits[Number(d)] + decimals[i]; 
+	            } 
+	        } 
+	    } 
+	    // Confirm and return the final output string:
+	    if (outputCharacters == "") { 
+	        outputCharacters = CN_ZERO + CN_DOLLAR; 
+	    } 
+	    if (decimal == "") { 
+	        outputCharacters += CN_INTEGER; 
+	    } 
+	    outputCharacters = outputCharacters; 
+	    return outputCharacters; 
+	} 
 }
 
 //弹框
